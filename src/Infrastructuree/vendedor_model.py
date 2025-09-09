9ce264eb34ebce53a82f5ad9152cede650691e96
ccsilva13 <caio.caetano@aluno.faculdadeimpacta.com.br>
2025-09-09 16:30:22 -0300

    Geração de id automatica e alteração na coluna celular

--- src/Infrastructuree/vendedor_model.py
+++ src/Infrastructuree/vendedor_model.py
@@ -1,13 +1,13 @@
-from src.config.database import db # Corrigido
+from src.config.database import db
 import uuid
 class VendedorModel(db.Model):
     __tablename__ = 'vendedores'
 
-    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(100), nullable=False)
     cnpj = db.Column(db.String(18), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    celular = db.Column(db.String(20), unique=True, nullable=False)
+    celular = db.Column(db.String(20), unique=False, nullable=False)
     senha = db.Column(db.String(200), nullable=False)
     status = db.Column(db.String(20), default="Inativo")
     codigo_ativacao = db.Column(db.String(4))
@@ -21,5 +21,3 @@ class VendedorModel(db.Model):
             "celular": self.celular,
             "status": self.status
         }
-        
-        
