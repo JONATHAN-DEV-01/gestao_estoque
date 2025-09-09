5362424bc9dde9b22f8ec005b0c75be4755253a6
JuanBernardini <bernardinijuan200@gmail.com>
2025-09-09 17:13:30 -0300

    Alteração da logica

--- run.py
+++ run.py
@@ -3,8 +3,9 @@ from src.config.database import db
 from src.routes import init_routes
 import os
 from dotenv import load_dotenv
+from src.Infrastructuree.vendedor_model import VendedorModel
 
-
+# Carrega as variáveis de ambiente no escopo global
 load_dotenv()
 
 # Cria a instância da aplicação no escopo global
@@ -13,7 +14,7 @@ app = Flask(__name__)
 # Configurações do banco de dados MySQL via variáveis de ambiente
 db_user = os.getenv('DB_USER')
 db_password = os.getenv('DB_PASSWORD')
-db_host = 'db'  # Nome do serviço no docker-compose.yml
+db_host = 'db'
 db_name = os.getenv('DB_NAME')
 
 # String de conexão do SQLAlchemy para MySQL
@@ -24,12 +25,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 init_routes(app)
 
-# Opcional: Para ambientes de produção, crie a tabela aqui
-# com app.app_context():
-#    db.create_all()
+# Executa db.create_all() ao iniciar a aplicação no contêiner
+# Este comando é executado sempre que o módulo é importado
+with app.app_context():
+    db.create_all()
 
+# O Gunicorn usará este objeto 'app'
+# O bloco 'if __name__ == '__main__'' é apenas para testes locais
 if __name__ == '__main__':
-    # Este bloco só será executado quando você rodar 'python run.py'
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
