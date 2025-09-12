bd72e52fd75d6db81ac2a72c3892dc1a89d1cbce
JuanBernardini <bernardinijuan200@gmail.com>
2025-09-11 22:24:48 -0300

    adição da rota JWT

--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,6 @@
-from flask import jsonify, make_response
+from flask import jsonify, make_response, request
 from src.application.controllers.vendedor_controller import VendedorController
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 def init_routes(app):
     @app.route('/api', methods=['GET'])
@@ -7,6 +8,10 @@ def init_routes(app):
         return make_response(jsonify({
             "mensagem": "API - OK; Docker - Up",
         }), 200)
+
+    @app.route('/login', methods=['POST'])
+    def login_vendedor():
+        return VendedorController.login_vendedor()
 
     @app.route('/vendedores', methods=['POST'])
     def create_vendedor():
@@ -17,17 +22,24 @@ def init_routes(app):
         return VendedorController.activate_vendedor()
 
     @app.route('/vendedores', methods=['GET'])
+    @jwt_required()
     def get_all_vendedores():
         return VendedorController.get_all_vendedores()
 
     @app.route('/vendedores/<int:vendedor_id>', methods=['GET'])
+    @jwt_required()
     def get_vendedor(vendedor_id):
+        # Lógica de validação do ID no controller
         return VendedorController.get_vendedor(vendedor_id)
 
     @app.route('/vendedores/<int:vendedor_id>', methods=['PUT'])
+    @jwt_required()
     def update_vendedor(vendedor_id):
+        # Lógica de validação do ID no controller
         return VendedorController.update_vendedor(vendedor_id)
 
     @app.route('/vendedores/<int:vendedor_id>', methods=['DELETE'])
+    @jwt_required()
     def delete_vendedor(vendedor_id):
+        # Lógica de validação do ID no controller
         return VendedorController.delete_vendedor(vendedor_id)
