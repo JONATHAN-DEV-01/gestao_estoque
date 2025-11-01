bc1b680eea2c37bd36d214179d9c985efeb1cdaa
JONATHAN-DEV-01 <jonathanads2006@gmail.com>
2025-11-01 19:49:57 -0300

    Alterações para o deploy

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,32 @@
-
+# Usa uma imagem base leve e segura
 FROM python:3.11-slim-bookworm
 
+# Define o diretório de trabalho dentro do contêiner
 WORKDIR /app
 
+# --- ALTERAÇÃO 1: SEGURANÇA ---
+# Cria um usuário não-root chamado 'appuser' para rodar a aplicação
+RUN useradd --create-home appuser
+# Define o proprietário do diretório de trabalho para o novo usuário
+RUN chown -R appuser:appuser /app
+
+# Copia o arquivo de dependências
 COPY requirements.txt .
+
+# Instala as dependências
 RUN pip install --no-cache-dir -r requirements.txt
 
+# Copia o restante do código da aplicação
 COPY run.py .
 COPY src ./src
 
+# --- MAIS DA ALTERAÇÃO 1: SEGURANÇA ---
+# Muda para o usuário não-root antes de iniciar a aplicação
+USER appuser
 
+# Expõe a porta que a aplicação vai usar
 EXPOSE 5000
 
-CMD ["gunicorn", "--bind=0.0.0.0:5000", "run:app"]
+# --- ALTERAÇÃO 2: PERFORMANCE ---
+# Comando para iniciar a aplicação com 3 workers para melhor performance
+CMD ["gunicorn", "--workers=3", "--bind=0.0.0.0:5000", "run:app"]
---
+++ supabase/migrations/20251028233056_create_initial_schema.sql
@@ -0,0 +1,65 @@
+
+
+
+CREATE TABLE vendedores (
+        id SERIAL NOT NULL, 
+        nome VARCHAR(100) NOT NULL, 
+        cnpj VARCHAR(18) NOT NULL, 
+        email VARCHAR(120) NOT NULL, 
+        celular VARCHAR(20) NOT NULL, 
+        senha VARCHAR(200) NOT NULL, 
+        status VARCHAR(20), 
+        codigo_ativacao VARCHAR(4), 
+        PRIMARY KEY (id), 
+        UNIQUE (cnpj), 
+        UNIQUE (email)
+)
+
+
+;
+
+
+CREATE TABLE produtos (
+        id SERIAL NOT NULL,
+        nome VARCHAR(100) NOT NULL,
+        preco FLOAT NOT NULL,
+        quantidade INTEGER NOT NULL,
+        status VARCHAR(20),
+        imagem VARCHAR(255),
+        vendedor_id INTEGER NOT NULL,
+        PRIMARY KEY (id),
+        FOREIGN KEY(vendedor_id) REFERENCES vendedores (id)
+)
+
+
+;
+
+
+CREATE TABLE vendas (
+        id SERIAL NOT NULL,
+        transacao_id VARCHAR(36) NOT NULL,
+        quantidade INTEGER NOT NULL,
+        preco_unitario_venda FLOAT NOT NULL,
+        data_venda TIMESTAMP WITHOUT TIME ZONE,
+        produto_id INTEGER NOT NULL,
+        vendedor_id INTEGER NOT NULL,
+        PRIMARY KEY (id),
+        FOREIGN KEY(produto_id) REFERENCES produtos (id),
+        FOREIGN KEY(vendedor_id) REFERENCES vendedores (id)
+)
+
+
+;
+
+
+CREATE TABLE token_blocklist (
+        id SERIAL NOT NULL,
+        jti VARCHAR(36) NOT NULL,
+        created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+        PRIMARY KEY (id),
+        UNIQUE (jti)
+)
+
+
+;
+
